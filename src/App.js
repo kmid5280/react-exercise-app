d52b8297c7e0177e4163c5832c821c45cb53e840
Kyle Middleton <kmiddleton654@gmail.com>
2019-04-04 20:51:35 -0600

    added authtoken code and router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {connect} from 'react-redux'
+import LandingPage from 'components/landing-page'
+import Header from 'components/header'
+import {refreshAuthToken} from 'src/actions/auth'
 
-class App extends Component {
+export class App extends React.Component {
   
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+      this.startPeriodicRefresh()
+    }
+    else if (prevProps.loggedIn && !this.props.loggedIn) {
+      this.stopPeriodicRefresh()
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()),
+      60*60*1000
+    )
+  }
+  
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return
+    }
+    clearInterval(this.refreshInterval)
+  }
   
   render() {
     return (
-      <div className="App">
-        <h1 class="title">ExerciseApp Placeholder</h1>
-      </div>
+      <Router>
+        <div className="app-wrapper">
+          <header>
+            <Header />
+          </header>
+          <main>
+            <Route exact path='/' component={LandingPage} />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  hasAuthToken: state.auth.authToken !== null,
+  loggedIn: state.auth.currentUser !== null
+})
+
+export default connect(mapStateToProps)(App);
