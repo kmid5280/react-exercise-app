2cc8eb45493f4da638855780f5f90b9afa6f0863
Kyle Middleton <kmiddleton654@gmail.com>
2019-04-04 20:52:04 -0600

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css'
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header>
+            <div className="header-wrapper">
+                <h1 className="header-title">Exercise App Header</h1>
+                
+            </div>
+            </header>
+        )
+    }
+}
