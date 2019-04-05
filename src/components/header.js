1bb7713e6e25468d9cdde475588b3fb04ca7c612
Kyle Middleton <kmiddleton654@gmail.com>
2019-04-05 12:05:14 -0600

    changed header component from class to function

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './header.css'
 
-export default class Header extends React.Component {
-    render() {
-        return (
-            <header>
+export default function Header() {
+    return (
+        <header>
             <div className="header-wrapper">
                 <h1 className="header-title">Exercise App Header</h1>
-                
             </div>
-            </header>
-        )
-    }
+        </header>
+        
+    )
 }
