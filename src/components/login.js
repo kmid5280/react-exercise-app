c259b08faf198166a61c03c3b04be1e9bf56ec8c
Kyle Middleton <kmiddleton654@gmail.com>
2019-04-05 19:19:12 -0600

    added username and password entry form

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {reduxForm, Field, SubmissionError, focus} from 'redux-form';
+import {Link} from 'react-router-dom';
+import './login.css'
+import {login} from '../actions/auth'
+import {required, nonEmpty} from '../validators';
 
-export default function Login() {
-    return (
-        <div>Placeholder text</div>
-    )
+export class Login extends React.Component {
+   
+    onSubmit(values) {
+        return this.props.dispatch(login(values))
+        .catch(err => {
+            throw new SubmissionError({_error: err.errors._error})
+        })
+    }
+    
+    render() {
+        let error;
+        if (this.props.error) {
+            
+            error = (
+                <div className="form-error" aria-live="polite">
+                    {this.props.error}
+                </div>
+            );
+        }
+
+        
+        return(
+            <main>
+                <div className="login-wrapper">
+                    <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+                    <Field component="input" placeholder="Enter username" name="username" type="text" id="login-username" className="login-username" validate={[required, nonEmpty]}/>
+                    <Field type="password" placeholder="Enter password" component="input" name="password" className="login-password" id="login-password" validate={[required, nonEmpty]}/>
+                    <button type="submit" className="login-submit-button">Sign in</button>
+                    <div className="error-message">{error}</div>
+                    <p className="login-no-account-text">Don't have an account? <Link className="signup-link" to='/signup'>Sign up!</Link></p>
+                    </form>
+                </div>
+            </main>
+        )
+    }
 }
+export default reduxForm({
+    form: 'login',
+    onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
+})(Login)
