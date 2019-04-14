8b5164cc68b35726b1c41b5f361c1f5528150cc6
Kyle Middleton <kylemid321@gmail.com>
2019-04-14 15:34:38 -0600

    initial commit

---
+++ src/components/signup.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './signup.css'
+import {connect} from 'react-redux'
+import {Link, Redirect} from 'react-router-dom'
+import SignupForm from './signup-form'
+
+export function Signup(props) {
+    if (props.loggedIn) {
+        return <Redirect to '/board' />
+    }
+    
+    return (
+        <main>
+            <SignupForm />
+            <Link className='login-link' to='/'>Main page</Link>
+        </main>
+    )
+
+}
+
+const mapStateToProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+})
+
+export default connect(mapStateToProps)(Signup)
