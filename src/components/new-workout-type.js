6700eb0fd6205e813cf939b488915277e876ecdf
Kyle Middleton <kylemid321@gmail.com>
2019-04-14 22:43:30 -0600

    moved onSubmit from current-workout-form to new-workout-type

--- src/components/new-workout-type.js
+++ src/components/new-workout-type.js
@@ -1,7 +1,18 @@
 import React from 'react'
-import { connect } from 'tls';
+import { connect } from 'react-redux';
+import {reduxForm, Field, SubmissionError, focus} from 'redux-form'
+import {newWorkoutType} from '../actions'
 
 export class NewWorkoutType extends React.Component {
+    onSubmit(values) {
+        const {userId, type, exercises} = values
+        return this.props.dispatch(newWorkoutType(values))
+        .catch(err => {
+            console.log({_error: err.errors._error})
+            throw new SubmissionError({_error: err.errors._error})
+        })
+    }
+    
     render() {
         return (
             <div>
