8ec32129309b5ddd6db3d8923888181f2d18f9d0
Kyle Middleton <kylemid321@gmail.com>
2019-04-14 22:47:16 -0600

    added NEW_WORKOUT_TYPE action and fixed errors

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,11 @@
 export const CURRENT_WORKOUT_FORM_SUBMIT = 'CURRENT_WORKOUT_FORM_SUBMIT'
-export const currentWorkoutFormSubmit = (type, exercises) => ({
+export const currentWorkoutFormSubmit = () => ({
     type: CURRENT_WORKOUT_FORM_SUBMIT
+})
+
+export const NEW_WORKOUT_TYPE = 'NEW_WORKOUT_TYPE'
+export const newWorkoutType = (workoutType, exercises) => ({
+    type: NEW_WORKOUT_TYPE,
+    workoutType,
+    exercises
 })
