582587a15eb1a2c94b423f345515521994e89c7f
Kyle Middleton <kylemid321@gmail.com>
2019-04-15 13:51:05 -0600

    added newWorkoutType reducer

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,6 +3,14 @@ import {SubmissionError} from 'redux-form';
 import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './utils';
 
+export const POST_NEW_WORKOUT_TYPE = 'POST_NEW_WORKOUT_TYPE'
+export const postNewWorkoutType = (user, workoutType, exercises) => ({
+    type: POST_NEW_WORKOUT_TYPE,
+    user,
+    workoutType,
+    exercises
+})
+
 export const registerUser = user => dispatch => {
     
     return fetch(`${API_BASE_URL}/users`, {
@@ -30,3 +38,32 @@ export const registerUser = user => dispatch => {
             }
         });
 };
+
+export const newWorkoutType = (userId, authToken, workoutType, exercises) => dispatch => {
+    const reqBody = {workoutType: workoutType, exercises: exercises}
+
+    return fetch(`${API_BASE_URL}/users/${userId}`, {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + authToken
+        },
+        body: JSON.stringify(reqBody)
+    })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(user => {
+        dispatch(postNewWorkoutType(user, workoutType, exercises))
+    })
+    .catch(err => {
+        const {reason, message, location} = err;
+        console.log(err)
+        if (reason === 'ValidationError') {
+            return Promise.reject(
+                new SubmissionError({
+                    [location]: message
+                })
+            )
+        }
+    })
+}
