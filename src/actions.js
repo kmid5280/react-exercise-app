60013849a09bb8cda9da8f39913839d6ad879e75
Kyle Middleton <kylemid321@gmail.com>
2019-04-15 13:53:14 -0600

    moved new workout type action to actions/users

--- src/actions.js
+++ src/actions.js
@@ -2,10 +2,3 @@ export const CURRENT_WORKOUT_FORM_SUBMIT = 'CURRENT_WORKOUT_FORM_SUBMIT'
 export const currentWorkoutFormSubmit = () => ({
     type: CURRENT_WORKOUT_FORM_SUBMIT
 })
-
-export const NEW_WORKOUT_TYPE = 'NEW_WORKOUT_TYPE'
-export const newWorkoutType = (workoutType, exercises) => ({
-    type: NEW_WORKOUT_TYPE,
-    workoutType,
-    exercises
-})
